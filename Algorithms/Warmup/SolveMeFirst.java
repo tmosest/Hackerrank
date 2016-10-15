844c6297b77e74be7158b78d1db5fb40ebfb90f7
Moses <AF17783@ad.wellpoint.com>
2016-10-14 23:47:56 -0400

    Added Solve Me First

---
+++ Algorithms/Warmup/SolveMeFirst.java
@@ -0,0 +1,23 @@
+package Warmup;
+import java.util.*;
+
+public class SolveMeFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+	     int a;
+	     a = in.nextInt();
+	     int b;
+	     b = in.nextInt();
+	     int sum;
+	     sum = solveMeFirst(a, b);
+	     System.out.println(sum);
+	}
+	
+	public static int solveMeFirst(int a, int b) {
+		// Hint: Type return a+b; below   
+		return a + b;
+	}
+
+}
