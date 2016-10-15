7640244ed755a079f0902caa48904b5efbbd3152
Moses <tmosest@gmail.com>
2016-10-15 01:09:29 -0400

    Added Staircase and test

---
+++ Algorithms/Warmup/Staircase.java
@@ -0,0 +1,33 @@
+package Warmup;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        String[] stairs = buildStairCase(n);
+        for(int i= 0; i < n; i++) {
+        	System.out.println(stairs[i]);
+        }
+	}
+	
+	public static String[] buildStairCase(int n)
+	{
+		String[] stairs = new String[n];
+		for(int i= 1; i <= n; i++) {
+            String s = "";
+            for(int j = 0; j < n; j++) {
+              if(j < n - i) {
+                  s += " ";
+              } else {
+                  s += "#";
+              }
+            }
+            stairs[i - 1] = s;
+        }
+		return stairs;
+	}
+}
