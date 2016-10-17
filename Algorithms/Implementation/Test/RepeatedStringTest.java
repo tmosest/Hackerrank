6b806bd45181654018bdedd92bda9f24ce504a22
Moses <tmosest@gmail.com>
2016-10-17 13:53:50 -0400

    Added commit about test failing

--- Algorithms/Implementation/Test/RepeatedStringTest.java
+++ Algorithms/Implementation/Test/RepeatedStringTest.java
@@ -15,6 +15,7 @@ public class RepeatedStringTest {
 		long result0 = RepeatedString.countAsInInfiniteString(s0, n0);
 		assertEquals(7, result0);
 		
+		//This test case is failing!
 		long n1 = Long.parseLong("1000000000000");
 		String s1 = "aba";
 		long result1 = RepeatedString.countAsInInfiniteString(s1, n1);
