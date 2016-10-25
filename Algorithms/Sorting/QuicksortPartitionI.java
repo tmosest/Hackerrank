ea0aadf6805f87ef87b425a4a34206249af4d949
Moses <tmosest@gmail.com>
2016-10-25 14:38:20 -0400

    Solved Quick Sort I Partition. No test cases.

--- Algorithms/Sorting/QuicksortPartitionI.java
+++ Algorithms/Sorting/QuicksortPartitionI.java
@@ -1,5 +1,54 @@
 package Sorting;
 
-public class QuicksortPartitionI {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+/**
+ *	Algorithms -> Sorting -> Running Time of Algorithms
+ *	Easy
+ */
+public class QuicksortPartitionI {
+	
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++){
+           ar[i]=in.nextInt(); 
+        }
+        in.close();
+        partition(ar);
+    }    
+	
+	static void partition(int[] ar) {
+		int pivot = ar[0];
+		ArrayList<Integer> left = new ArrayList<Integer>();
+		ArrayList<Integer> right = new ArrayList<Integer>();
+		for(int i = 1; i < ar.length; i++) {
+			if(ar[i] > pivot) {
+				right.add(ar[i]);
+			} else {
+				left.add(ar[i]);
+			}
+		}
+		int i = 0;
+		for(int j : left) {
+			ar[i] = j;
+			i++;
+		}
+		ar[i] = pivot;
+		i++;
+		for(int j : right) {
+			ar[i] = j;
+			i++;
+		}
+		printArray(ar);
+    }   
+
+	static void printArray(int[] ar) {
+		for(int n: ar){
+			System.out.print(n+" ");
+	    }
+		System.out.println("");
+	}
 }
