52cfecddd184aac46534dbfe92e61b20c81f0c94
Moses <tmosest@gmail.com>
2016-11-08 10:47:38 -0500

    Added New York Chaos. Haven't solved it yet.

---
+++ Algorithms/Constructive/NewYearChaos.java
@@ -0,0 +1,25 @@
+package Constructive;
+
+/**
+ *	Algorithms -> Constructive Algorithms -> New Year Chaos 
+ *	Medium
+ *
+ *	Sample Input:
+ 	2
+	5
+	2 1 5 3 4
+	5
+	2 5 1 3 4
+ *
+ *	Sample Output:
+ 	3
+	Too chaotic
+ *
+ */
+public class NewYearChaos {
+
+	public static void main(String[] args) {
+
+	}
+
+}
