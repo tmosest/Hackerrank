1dc44e5e4467e2bb0715508fed80c0a7ddbd7237
Moses <tmosest@gmail.com>
2016-11-09 22:16:32 -0500

    Solved Lonely Integer. No test cases.

--- Algorithms/BitManipulation/LonelyInteger.java
+++ Algorithms/BitManipulation/LonelyInteger.java
@@ -1,5 +1,8 @@
 package BitManipulation;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  *	Algorithms -> Bit Manipulation -> Lonely Integer
  *	Easy
@@ -7,8 +10,26 @@ package BitManipulation;
 public class LonelyInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		HashMap<Integer, Integer> numCount = new HashMap();
+		
+		int arraySize = in.nextInt();
+		for(int i = 0; i < arraySize; i++) {
+			int number = in.nextInt();
+			int numberCount = (numCount.get(number) == null) ? 0 : numCount.get(number);
+			numCount.put(number, ++numberCount);
+		}
+		
+		for(int number : numCount.keySet()) {
+			int count = numCount.get(number);
+			if(count == 1) {
+				System.out.println(number);
+				break;
+			}
+		}
+		
+		in.close();
 	}
-
+	
 }
