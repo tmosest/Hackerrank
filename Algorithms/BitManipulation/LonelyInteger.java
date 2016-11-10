5de574e82f528e78364b01562892f60043a16858
Moses <tmosest@gmail.com>
2016-11-09 22:11:11 -0500

    Cleaned up Project.

---
+++ Algorithms/BitManipulation/LonelyInteger.java
@@ -0,0 +1,14 @@
+package BitManipulation;
+
+/**
+ *	Algorithms -> Bit Manipulation -> Lonely Integer
+ *	Easy
+ */
+public class LonelyInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
