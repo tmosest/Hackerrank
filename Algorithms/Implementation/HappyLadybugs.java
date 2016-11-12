845c9f4191bc1a1917eaee0e167df36a0f1eec07
Moses <tmosest@gmail.com>
2016-11-11 20:06:13 -0500

    Still working on Happy Lady Bugs.

--- Algorithms/Implementation/HappyLadybugs.java
+++ Algorithms/Implementation/HappyLadybugs.java
@@ -9,100 +9,63 @@ import java.util.Scanner;
  *	Easy
  */
 public class HappyLadybugs {
-
+	
 	public static boolean isSame(String str) {
-		boolean isSame = true;
-
-		for (int i = 1; i < str.length() - 1; i++) {
-			if (str.charAt(i) != str.charAt(i - 1)) {
-				isSame = false;
-				break;
-			}
-		}
-		return isSame;
+		 boolean isSame = true;
+		 
+		 for (int i = 1; i < str.length() - 1; i++) {
+			 if (str.charAt(i) != str.charAt(i - 1)) {
+				 isSame = false;
+		 		break;
+		 	 }
+		 }
+		 return isSame;
 	}
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 
-		int t = scan.nextInt();
+		int numCases = in.nextInt();
 
-		for (int i = 0; i < t; i++) {
-
-			int n = scan.nextInt();
-			scan.nextLine();
-			String str = scan.nextLine();
-
-			// only one underscore
-			if (str.length() == 1 && str.charAt(0) == '_') {
-				System.out.println("YES");
-			}
-			// all same
-			else if (isSame(str)) {
-				System.out.println("YES");
-			} else {
-
-				// construct map
-
-				HashMap<String, Integer> map = new HashMap();
-				for (int j = 0; j < n; j++) {
-					String s = String.valueOf(str.charAt(j));
-					if (map.containsKey(s)) {
-						int val = map.get(s);
-						val++;
-						map.remove(s);
-						map.put(String.valueOf(s), val);
-					} else {
-						map.put(String.valueOf(s), 1);
-					}
-
+		for(int i = 0; i < numCases; i++) {
+			int arraySize = in.nextInt();
+			
+			HashMap<Character, Integer> wordCount = new HashMap();
+			
+			String bugArray = in.next();
+			boolean hasUnderscore = false;
+			for(int a_i = 0; a_i < arraySize; a_i++) {
+				char bugLetter = bugArray.charAt(a_i);
+				if(bugLetter == '_') {
+					hasUnderscore = true;
 				}
-
-				// System.out.println(map);
-
-				int flag = 0;
-				for (Entry<String, Integer> string : map.entrySet()) {
-
-					if (flag == 1)
-						break;
-					String key = string.getKey();
-					int value = string.getValue();
-
-					// underscore is there but any char is only one
-
-					if (value == 1
-							&& !string.getKey().equals(String.valueOf('_'))) {
-						System.out.println("NO");
-						flag = 1;
-						break;
-					} else {
-						for (int g = 1; g < n - 1; g++) {
-							if (str.charAt(g) == str.charAt(g - 1)
-									|| str.charAt(g) == str.charAt(g + 1)) {
-							} else {
-								if (str.contains(String.valueOf('_'))) {
-
-								} else {
-									if (map.size() == 1) {
-									} else {
-										System.out.println("NO");
-										flag = 1;
-										break;
-									}
-
-								}
-							}
+				int letterCount = (wordCount.get(bugLetter) == null) ? 0 : wordCount.get(bugLetter);
+				wordCount.put(bugLetter, ++letterCount);
+			} //end for a_i
+			
+			boolean canBeHappy = true;
+			if(!hasUnderscore) {
+				canBeHappy = isSame(bugArray);
+			} else {
+				for(char letter: wordCount.keySet()) {
+					if(letter != '_') {
+						int count = wordCount.get(letter);
+						if(count < 2) {
+							canBeHappy = false;
+							break;
 						}
 					}
-
 				}
-				if (flag == 0)
-					System.out.println("YES");
-
 			}
-
-		} // for closes
-
+			
+			String output = "NO";
+			if(canBeHappy) output = "YES";
+			
+			System.out.println(output);
+			
+		} //end for i
+		
+		in.close();
 	}
 
 }
