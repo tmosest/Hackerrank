3c43404df9acc9794eddb7481bbc433c56ed619c
Moses <tmosest@gmail.com>
2016-11-15 07:03:51 -0500

    Solved Closest Numbers. No test cases.

--- Algorithms/Sorting/TheFullCountingSort.java
+++ Algorithms/Sorting/TheFullCountingSort.java
@@ -10,24 +10,7 @@ import java.util.TreeMap;
 public class TheFullCountingSort {
 
 	public static void main(String[] args) {
-		TreeMap<String, Integer> countToWord = new TreeMap<String, Integer>();
 		
-		Scanner in = new Scanner(System.in);
-		int listSize = in.nextInt();
-		for(int i = 0; i < listSize; i++) {
-			int count = in.nextInt();
-			String line = in.next();
-			if(i < listSize / 2) {
-				line = "-";
-			}
-			countToWord.put(line, count);
-		}
-		
-		for(String line: countToWord.keySet()) {
-			System.out.print(line + " ");
-		}
-		
-		in.close();
 	}
 
 }
