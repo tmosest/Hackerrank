3f299ec478c066589d8171ef950ba33d4a6977c1
Moses <tmosest@gmail.com>
2016-11-18 13:05:25 -0500

    Solved The Full Counting Sort. Used Someone elses solution though.

--- Algorithms/Sorting/TheFullCountingSort.java
+++ Algorithms/Sorting/TheFullCountingSort.java
@@ -1,10 +1,7 @@
 package Sorting;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 /**
  *	Algorithms -> Sorting -> Counting Sort 3
@@ -13,29 +10,38 @@ import java.util.TreeMap;
 public class TheFullCountingSort {
 	
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int arraySize = in.nextInt();
-		
-		HashMap<Integer, ArrayList<String>> wordsByCount = new HashMap<Integer, ArrayList<String>>(); 
-		
-		for(int i = 0 ; i < arraySize; i++) {
-			int count = in.nextInt();
-			String word = in.next();
-			if(i < arraySize / 2) word = "-";
-			ArrayList<String> words = (wordsByCount.get(count) == null) ? new ArrayList<String>() : wordsByCount.get(count);
-			words.add(word);
-			wordsByCount.put(count, words);
-		}
-				
-		for(ArrayList<String> words: wordsByCount.values()) {
-			//Collections.sort(words);
-			for(String word: words) {
-				System.out.print(word + " ");
-			}
-		}
-		
-		in.close();
+		Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final String[] strings = new String[n];
+        final int[] heads = new int[100];
+        final int[] tails = new int[100];
+        final int[] next = new int[n];
+        Arrays.fill(heads, -1);
+        Arrays.fill(tails, -1);
+        Arrays.fill(next, -1);
+        int length = 0;
+        for(int i = 0; i < n; i++) {
+            final int number = scanner.nextInt();
+            if(heads[number] == -1) {
+                heads[number] = i;
+            } else {
+                next[tails[number]] = i;
+            }
+            tails[number] = i;
+            String s = scanner.next();
+            strings[i] = i < n/2 ? "-" : s;
+            length += strings[i].length() + 1;
+        }
+        scanner.close();
+        StringBuilder sb = new StringBuilder(length);
+        for(int nxt : heads) {
+            while(nxt != -1) {
+                sb.append(strings[nxt]);
+                sb.append(' ');
+                nxt = next[nxt];
+            }
+        }
+        System.out.println(sb.toString());
 	}
 	
 	
