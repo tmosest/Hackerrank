f15d9ac184e2f0f2394c068efbf5100ee0753b37
Moses <tmosest@gmail.com>
2016-11-18 13:02:03 -0500

    Passed one test case of The Full Counting Sort.

--- Algorithms/Sorting/TheFullCountingSort.java
+++ Algorithms/Sorting/TheFullCountingSort.java
@@ -1,5 +1,8 @@
 package Sorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -8,9 +11,32 @@ import java.util.TreeMap;
  *	Medium
  */
 public class TheFullCountingSort {
-
+	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		int arraySize = in.nextInt();
+		
+		HashMap<Integer, ArrayList<String>> wordsByCount = new HashMap<Integer, ArrayList<String>>(); 
+		
+		for(int i = 0 ; i < arraySize; i++) {
+			int count = in.nextInt();
+			String word = in.next();
+			if(i < arraySize / 2) word = "-";
+			ArrayList<String> words = (wordsByCount.get(count) == null) ? new ArrayList<String>() : wordsByCount.get(count);
+			words.add(word);
+			wordsByCount.put(count, words);
+		}
+				
+		for(ArrayList<String> words: wordsByCount.values()) {
+			//Collections.sort(words);
+			for(String word: words) {
+				System.out.print(word + " ");
+			}
+		}
+		
+		in.close();
 	}
-
+	
+	
 }
