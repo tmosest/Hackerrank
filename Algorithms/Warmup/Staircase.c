062e5d396ba961b43ae3f8194a570efd75e3a540
Moses <tmosest@gmail.com>
2016-11-22 16:29:55 -0500

    Solved Staircase. This was even easier the second time around.

---
+++ Algorithms/Warmup/Staircase.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+/**
+ *	Algorithms -> Warmup -> Plus Minus
+ *	Easy (Super Easy)
+ */
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i = 1; i <= n; i++) {
+    	for(int j = n; j > i; j--) {
+    		printf(" ");
+    	}
+    	for(int j = 1; j <= i; j++) {
+    		printf("#");
+    	}
+    	printf("\n");
+    }
+
+    return 0;
+}
