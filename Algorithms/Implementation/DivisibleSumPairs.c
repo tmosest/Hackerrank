392b9cf6e8902f58f0d18b153936b0b8732be19e
Moses <tmosest@gmail.com>
2016-11-23 17:42:18 -0500

    Solved Divisible Sum Pairs.

--- Algorithms/Implementation/DivisibleSumPairs.c
+++ Algorithms/Implementation/DivisibleSumPairs.c
@@ -4,3 +4,27 @@
  *	Algorithms -> Implementation -> Divisible Sum Pairs
  *	Easy
  */
+
+int main() {
+	int arraySize, mod;
+
+	scanf("%d %d", &arraySize, &mod);
+
+	int array[arraySize];
+
+	for(int i = 0; i < arraySize; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	int pairCount = 0;
+
+	for(int i = 0; i < arraySize - 1; i++) {
+		for(int j = i + 1; j < arraySize; j++) {
+			if( (array[i] + array[j]) % mod == 0 ) pairCount += 1;
+		}
+	}
+
+	printf("%d", pairCount);
+
+	return 0;
+}
