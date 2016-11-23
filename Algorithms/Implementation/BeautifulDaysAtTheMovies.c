0e956b88df8663cc66b03a67b034b42f6b0a030a
Moses <tmosest@gmail.com>
2016-11-23 12:16:33 -0500

    Solved Beautiful Day At The Movies.

--- Algorithms/Implementation/BeautifulDaysAtTheMovies.c
+++ Algorithms/Implementation/BeautifulDaysAtTheMovies.c
@@ -10,7 +10,43 @@
  *	Algorithms -> Warmup -> Beautiful Days at the Movies
  *	Easy
  */
+
+// Takes an integer and returns the reverse of it
+int reverseInteger(int number) {
+	int reverse = 0;
+	int n = number;
+	while (n != 0) {
+		reverse = reverse * 10;
+	    reverse = reverse + n % 10;
+	    n = n / 10;
+	}
+	//printf("reverse: %d\n", reverse);
+	return reverse;
+}
+
+//Checks if a day is beautiful mod k
+int isDayBeautiful(int number, int mod) {
+	int result = 0;
+	//printf("abs(number - reverseInteger(number)) mod: %d\n", abs(number - reverseInteger(number)) % mod);
+	if(abs(number - reverseInteger(number)) % mod == 0) {
+		result = 1;
+	}
+	return result;
+}
+
 int main(){
+	int startDay, endDay, divisibilityFactor;
+
+	scanf("%d %d %d", &startDay, &endDay, &divisibilityFactor);
+
+	int beautifulCount = 0;
+
+	for(int day = startDay; day <= endDay; day++) {
+		beautifulCount += isDayBeautiful(day, divisibilityFactor);
+	}
+
+	printf("%d", beautifulCount);
 
     return 0;
 }
+
