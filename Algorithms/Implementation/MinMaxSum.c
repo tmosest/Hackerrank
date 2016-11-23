de48f862b484a6af09f5ef2dd44c39e0aa7384a9
Moses <tmosest@gmail.com>
2016-11-23 11:11:36 -0500

    Solved Mini Max Sum. No test cases.

---
+++ Algorithms/Implementation/MinMaxSum.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+/**
+ *	Algorithms -> Warmup -> A Very Big Sum
+ *	Easy (Super Easy)
+ */
+int main(){
+	long long a, b, c, d, e;
+
+	scanf("%lld %lld %lld %lld %lld",&a,&b,&c,&d,&e);
+
+	long long array[] = {a, b, c, d, e};
+
+	long long min = a + b + c + d + e;
+	long long max = 0;
+
+	for(int i = 0; i < 5; i++) {
+		long long sum = 0;
+		for(int j = 0; j < 5; j++) {
+			if(i != j) sum += array[j];
+		}
+		if(sum > max) max = sum;
+		if(sum < min) min = sum;
+	}
+
+	printf("%lld %lld", min, max);
+
+    return 0;
+}
