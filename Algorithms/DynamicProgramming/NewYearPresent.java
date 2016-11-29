7cc3605fa7b319e6ab02322a8f5ea077af3d50b6
Moses <tmosest@gmail.com>
2016-11-29 08:31:55 -0500

    Cleaned project.

---
+++ Algorithms/DynamicProgramming/NewYearPresent.java
@@ -0,0 +1,17 @@
+package DynamicProgramming;
+
+import java.util.Scanner;
+
+/**
+ *	Algorithms -> Dynamic Programming -> New Year Present 
+ *	Hard
+ */
+public class NewYearPresent {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		in.close();
+	}
+
+}
