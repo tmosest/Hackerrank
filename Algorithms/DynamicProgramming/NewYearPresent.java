1347869bad82b75a91cefd2b105a54c4adce4178
Moses <tmosest@gmail.com>
2016-11-29 08:51:33 -0500

    Started working on New Year Present.

--- Algorithms/DynamicProgramming/NewYearPresent.java
+++ Algorithms/DynamicProgramming/NewYearPresent.java
@@ -7,9 +7,31 @@ import java.util.Scanner;
  *	Hard
  */
 public class NewYearPresent {
-
+	
+	static int maxSizeForArray = (int) (2 * 1e7 + 5);
+	
+	static long dp1[] = new long[maxSizeForArray];
+	static long dp2[] = new long[maxSizeForArray];
+	static long dp3[] = new long[maxSizeForArray];
+	
+	static long ans, ans1, ans2, ans3, pom;
+	
+	static int arraySize, idx;
+	
+	static int[] sticks;
+	
 	public static void main(String[] args) {
+		
 		Scanner in = new Scanner(System.in);
+		
+		arraySize = in.nextInt();
+		sticks = new int[arraySize];
+		
+		for(int i = 0; i < arraySize; i++) {
+			sticks[i] = in.nextInt();
+		}
+		
+		
 		
 		in.close();
 	}
