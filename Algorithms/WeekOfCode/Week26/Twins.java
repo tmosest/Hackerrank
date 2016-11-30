aefeb802c8b14255af3df0305f0465c79e353932
Moses <tmosest@gmail.com>
2016-11-30 16:11:48 -0500

    Added Twins need to solve it.

---
+++ Algorithms/WeekOfCode/Week26/Twins.java
@@ -0,0 +1,17 @@
+package WeekOfCode.Week26;
+
+import java.util.Scanner;
+
+/**
+ *	Algorithms -> Week of Code 26 -> Twins
+ *	Medium
+ */
+public class Twins {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		in.close();
+	}
+
+}
