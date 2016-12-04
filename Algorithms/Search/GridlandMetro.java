63f534fb4b30239334577314483215a60f066532
Moses <tmosest@gmail.com>
2016-12-03 20:13:22 -0500

    Gridland Metro: Got closer, don't time out but fail a lot of test cases.

--- Algorithms/Search/GridlandMetro.java
+++ Algorithms/Search/GridlandMetro.java
@@ -1,6 +1,8 @@
 package Search;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -19,6 +21,56 @@ import java.util.Scanner;
  */
 public class GridlandMetro {
 
+	private static class Tuple implements Comparator<Tuple>, Comparable<Tuple> {
+		public int x1;
+		public int x2;
+		
+		Tuple(int x, int y) {
+			this.x1 = x;
+			this.x2 = y;
+		}
+
+		/*
+		 * Copies the comparison for Python tuples
+		 */
+		@Override
+		public int compare(Tuple lhs, Tuple rhs) {
+			//TODO return 1 if rhs should be before lhs 
+		    //     return -1 if lhs should be before rhs
+		    //     return 0 otherwise
+			if(lhs.x1 < rhs.x1) {
+				return -1;
+			} else if(lhs.x1 > rhs.x1) {
+				return 1;
+			} else {
+				if(lhs.x2 < rhs.x2) {
+					return -1;
+				} else if(lhs.x2 > rhs.x2) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		}
+
+		@Override
+		public int compareTo(Tuple t) {
+			if(this.x1 < t.x1) {
+				return -1;
+			} else if(this.x1 > t.x1) {
+				return 1;
+			} else {
+				if(this.x2 < t.x2) {
+					return -1;
+				} else if(this.x2 > t.x2) {
+					return 1;
+				} else {
+					return 0;
+				}
+			} 
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
@@ -27,45 +79,53 @@ public class GridlandMetro {
 		
 		int numTrainTracks = in.nextInt();
 		
-		int numLamps = rows * columns;
-		
-		HashMap<Integer, int[]> trackKeeper = new HashMap<Integer, int[]>();
+		HashMap<Integer, ArrayList<Tuple>> tracks = new HashMap<Integer, ArrayList<Tuple>> ();
 		
 		for(int j = 0; j < numTrainTracks; j++) {
 			
 			int row = in.nextInt();
 			int start = in.nextInt();
 			int end = in.nextInt();
-			int diff = 0;
-			int[] track = (trackKeeper.get(row) == null) ? new int[2] : trackKeeper.get(row);
 			
-			if(track[0] == 0) {
-				track[0] = start;
-				diff = (end + 1) - start;
-			} else {
-				if(start < track[0]) {
-					diff += (track[0] - start); 
-					track[0] = start;
-				}
-			}
-			if(track[1] == 0) {
-				track[1] = end;
-			} else {
-				if(end > track[1]) {
-					diff += (end - track[1]); 
-					track[1] = end;
-				}
+			Tuple cordinates = new Tuple(start, end);
+			ArrayList<Tuple> track = tracks.get(row);
+			
+			if(track == null) {
+				track = new ArrayList<Tuple>();
 			}
 			
-			numLamps -= diff;
-			
-			//System.out.println("row: " + row + " track[0]: " + track[0] + " track[1]: " + track[1] + " diff: " + diff + " lamps: " + numLamps);
-			
-			trackKeeper.put(row, track);
+			track.add(cordinates);
+			tracks.put(row, track);
 		}
 		
-		in.close();
-
+        in.close();
+        
+		int numLamps = rows * columns;
+		int gridPoints = 0;
+		
+		for(int row : tracks.keySet()) {
+			ArrayList<Tuple> temp = tracks.get(row);
+			Collections.sort(temp);
+			
+			int begin = temp.get(0).x1;
+			int end = temp.get(0).x2;
+			int points = 0;
+			
+			for(int i = 1; i < temp.size(); i++) {
+				if(temp.get(i).x1 > end) {
+					points += end - begin + 1;
+					begin = temp.get(i).x1;
+					end = temp.get(i).x2;
+				} else {
+					end = Math.max(end, temp.get(i).x2);
+				}
+			}
+			points += end - begin + 1;
+			gridPoints += points;
+		}
+		
+		numLamps -= gridPoints;
+		
 		System.out.println(numLamps);
 	}
 }
