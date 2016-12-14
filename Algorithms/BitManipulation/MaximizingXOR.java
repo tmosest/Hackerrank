a8ab362bf1cee25ee1add74dcb1109e2a6a41a2d
Moses <tmosest@gmail.com>
2016-12-14 08:34:06 -0500

    Maximizing XOR: Have C++ solution need to convert to Java.

---
+++ Algorithms/BitManipulation/MaximizingXOR.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+/**
+ *	Algorithms -> Bit Manipulation -> Maximizing XOR
+ *	Easy
+ */
+public class MaximizingXOR {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
