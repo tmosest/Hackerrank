00d31df9cd0ae1ce4436510bc8d900c16cabdca2
Moses <tmosest@gmail.com>
2016-12-15 07:11:50 -0500

    Project 10: Pretty sure I solved it this time and I can use this approach to solve 7.

--- Algorithms/ProjectEuler/Project10.java
+++ Algorithms/ProjectEuler/Project10.java
@@ -11,6 +11,7 @@ public class Project10 {
 	private static boolean debugMode = false;
 	
 	private static boolean[] seive;
+	private static long[] seiveSum;
 	private static long primeSum;
 	
 	public static void main(String[] args) {
@@ -18,30 +19,34 @@ public class Project10 {
 		
 		int cases = in.nextInt();
 		
+		fillSieve();
+		
 		for(int c = 0; c < cases; c++) {
 			int number = in.nextInt();
-			fillSieve(number);
-			System.out.println(primeSum);
+			long sum = seiveSum[number];
+			System.out.println(sum);
 		}
 		
 		in.close();
 	}
 	
-	public static void fillSieve(int number) 
+	public static void fillSieve() 
 	{	
-		seive = new boolean[number + 1];
+		seive = new boolean[10001];
+		seiveSum = new long[10001];
 		primeSum = 0;
 		
 		seive[0] = true;
 		seive[1] = true;
 		
-		for(int i = 2; i <= number; i++) {
+		for(int i = 2; i <= 10000; i += 1) {
 			if(!seive[i]) {
 				primeSum += i;
-				for(int j = 1; j * i <= number; j++) {
+				for(int j = 1; j * i <= 10000; j++) {
 					seive[i * j] = true;
 				}
 			}
+			seiveSum[i] = primeSum;
 		}
 	}
 }
