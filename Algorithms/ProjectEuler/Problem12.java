203de26ed87deed4f5f46429d8d9b94ab45924a6
Moses <tmosest@gmail.com>
2016-12-16 12:08:35 -0500

    Problem 12: Started.

---
+++ Algorithms/ProjectEuler/Problem12.java
@@ -0,0 +1,17 @@
+package ProjectEuler;
+
+import java.util.Scanner;
+
+/**
+ * Project Euler #12: Highly divisible triangular number 
+ * Easy
+ */
+public class Problem12 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		in.close();
+	}
+
+}
