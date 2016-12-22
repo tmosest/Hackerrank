2817e9159b5b99be204391c3efbb1723c624b5b8
Moses <tmosest@gmail.com>
2016-12-22 17:29:02 -0500

    Zero Move Nim: Solved it finally.

--- Algorithms/WeekOfCode/Week27/ZeroMoveNim.java
+++ Algorithms/WeekOfCode/Week27/ZeroMoveNim.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 /**
  *	Algorithms -> Week of Code 27 -> Zero-Move Nim
  *	Medium
+ *
+ *Game Theory:
+ *https://web.stanford.edu/class/cs97si/05-combinatorial-games.pdf
+ *https://www.topcoder.com/community/data-science/data-science-tutorials/algorithm-games/
+ *http://www.cs.ubbcluj.ro/~moltean/oltean_nim.pdf
+ *http://letuskode.blogspot.com.br/2014/08/grundy-numbers.html
+ *http://math.stackexchange.com/questions/416042/why-xor-operator-works
+ *http://web.mit.edu/sp.268/www/nim.pdf
+ *https://www.youtube.com/watch?v=Owz6aNzGID4
+ *https://www.youtube.com/watch?v=Pys4p6qPnFE
  */
 public class ZeroMoveNim {
 
@@ -17,10 +27,15 @@ public class ZeroMoveNim {
 			
 			int numOfStacks = in.nextInt();
 			
-			int[] stacksOfStones = new int[numOfStacks];
+			long[] stacksOfStones = new long[numOfStacks];
 			
 			for(int s = 0; s < numOfStacks; s++) {
-				stacksOfStones[s] = in.nextInt();
+				stacksOfStones[s] = in.nextLong();
+				if(stacksOfStones[s] != 0 && stacksOfStones[s] % 2 == 0) {
+					stacksOfStones[s]--;
+				} else if(stacksOfStones[s] != 0 && stacksOfStones[s] % 2 == 1) {
+					stacksOfStones[s]++;
+				}
 			}
 			
 			int winner = GameOfNim.determineWinnderOfGame(stacksOfStones);
@@ -31,24 +46,21 @@ public class ZeroMoveNim {
 		
 		in.close();
 	}
-
+	
 	private static class GameOfNim
 	{
-		private static int determineWinnderOfGame(int[] stacks)
+		
+		public static int determineWinnderOfGame(long[] stacks)
 		{
 			int winner = 1;
 			
-			int stackXOR = stacks[0];
+			long stackXOR = stacks[0];
 			
 			for(int i = 1; i < stacks.length; i++) {
 				stackXOR ^= stacks[i];
 			}
 			
 			if(stackXOR == 0) winner = 2;
-			
-			if(stacks.length % 2 == 1) {
-				winner = (winner == 1) ? 2 : 1;
-			}
 			
 			return winner;
 		}
