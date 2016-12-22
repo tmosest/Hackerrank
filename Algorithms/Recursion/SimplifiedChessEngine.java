7ce9111843bccbf474ed2d2b6b00e6cc7c8da9ac
Moses <tmosest@gmail.com>
2016-12-22 17:57:35 -0500

    Setup branch

---
+++ Algorithms/Recursion/SimplifiedChessEngine.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+/**
+ *	Algorithms -> Recursion -> Simplified Chess Engine 
+ *	Medium
+ */
+public class SimplifiedChessEngine {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
