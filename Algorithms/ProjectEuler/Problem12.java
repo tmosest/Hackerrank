ffc9ec44653e22f210ee8df88cc13ce7813a6948
Moses <tmosest@gmail.com>
2017-01-01 18:01:16 -0500

    Problem 12: solved. There was a trick by looking at the divisors of n and n + 1 which are coprime.

--- Algorithms/ProjectEuler/Problem12.java
+++ Algorithms/ProjectEuler/Problem12.java
@@ -9,64 +9,65 @@ import java.util.Scanner;
 public class Problem12 {
 	private static boolean debugMode = false;
 	
-	private static int numberOfDivisorsLimit = 1000;
-	private static int[] numberOfDivisors = new int[numberOfDivisorsLimit + 1];
-	private static int[] primes = {2,4,6,9,16,18,20,24,36,40,48,90,112,128,144,162,168,192,240,320,480,576,648,768,1024};
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		generateTriangleDivisors();
-		
+				
 		int cases = in.nextInt();
 		
 		for(int c = 0; c < cases; c++) {
 			int divisorsToCheckFor = in.nextInt();
-			System.out.println(findDivisors(divisorsToCheckFor));
+			int n = first_triangle_with_more_than_n_divisors(divisorsToCheckFor);
+			System.out.println(triangleNumber(n));
 		}
 		
 		in.close();
 	}
 	
-	private static int findDivisors(int num) {
-		for(int i = num; i < numberOfDivisorsLimit; i++)
-			if(numberOfDivisors[i] != 0) return numberOfDivisors[i];
-		return num;
-	}
-	
-	public static void generateTriangleDivisors()
+	// triangle(n) = (n + 1) * n / 2
+	// only one of them has a factor of two, and they have no prime factors in common
+	// we can cache one of the divisor counts while iterating over triangle numbers
+	private static int first_triangle_with_more_than_n_divisors(int n)
 	{
-		int numDivisors = 0;
-		int nthTriangleNumber = 1;
-		int triangleNumber = 0;
-		while(numDivisors <= numberOfDivisorsLimit) {
-			triangleNumber = triangleNumber(nthTriangleNumber);
-			numDivisors = countDivisors(triangleNumber);
-			if(debugMode)
-				System.out.println("triangle num: " + triangleNumber + " #divisors: " + numDivisors);
-			if(numDivisors <= numberOfDivisorsLimit && numberOfDivisors[numDivisors] == 0) {
-				numberOfDivisors[numDivisors] = triangleNumber;
-			}
-			nthTriangleNumber++;
-		}
+	    int i = 1;
+	    for (int f1 = unique_divisors(i), f2 = unique_divisors(i + 1); f1 * f2 <= n; ++i)   {
+	        f1 = f2;                        // re-use result from previous iteration
+	        f2 = unique_divisors(i + 2);    // only one new computation
+	    }
+	    return i;
 	}
+
 	
 	private static int countDivisors(int num)
 	{
-		if(num % 2 == 0) num = num / 2;
-		
-		int divisors = 1;
-		int count = 0;
-		
-		for(int prime : primes) {
-			while(num % prime == 0) {
-				count += 1;
-				num = num / prime;
-			}
-			if(num == 0) break;
-		}
-		
-		return divisors;
+		int divisors = 1;  // 1 has one divisor
+	    {
+	    	int f = 2;
+	    	int count = 0;
+	        while (num % f == 0) {
+	            ++count;
+	            num /= f;     // divide by factor of 2
+	        }
+	        divisors *= (count + 1);
+	    }
+
+	    // <= n guarantees that a prime has 2 divisors
+	    for (int f = 3; f <= num; f += 2) {
+	    	int count = 0;
+	        while (num % f == 0) {
+	            ++count;
+	            num /= f;     // divide by odd factor
+	        }
+	        divisors *= (count + 1);
+	    }
+	    return divisors;
+	}
+	
+	private static int unique_divisors(int n)
+	{
+	    if (n % 2 == 0)
+	        n /= 2;
+	    return countDivisors(n);
 	}
 	
 	/**
