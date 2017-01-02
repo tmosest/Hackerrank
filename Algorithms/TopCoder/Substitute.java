0652546f6b80dde2655a05b7663e2e7f55fb941d
Moses <tmosest@gmail.com>
2017-01-01 23:21:24 -0500

    Substitue: solved but only got 181.89 points.

---
+++ Algorithms/TopCoder/Substitute.java
@@ -0,0 +1,35 @@
+package TopCoder;
+
+/**
+ * TopCoder -> Practice -> Substitute
+ * Easy/Single/250
+ * @author tmosest
+ *
+ */
+public class Substitute {
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public int getValue(String key, String code)
+	{
+		int value = 0;
+		
+		for(int i = 0; i < code.length(); i++) {
+			char letter = code.charAt(i);
+			for(int j = 0; j < key.length(); j++) {
+				if(letter == key.charAt(j)) {
+					value *= 10;
+					if(j != key.length() - 1) {
+						value += (j + 1);
+					}
+				}
+			}
+		}
+		
+		return value;
+	}
+	
+}
