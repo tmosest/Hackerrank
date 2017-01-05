3e5fd7b922db489d9996cf426af77c7c4e67eda5
Moses <tmosest@gmail.com>
2017-01-05 15:47:31 -0500

    Reverse Shuffle Merge: solved a few more test cases.

--- Algorithms/Greedy/ReverseShuffleMerge.java
+++ Algorithms/Greedy/ReverseShuffleMerge.java
@@ -38,25 +38,57 @@ public class ReverseShuffleMerge {
 		if(debugMode) 
 			System.out.println("smallest: " + smallest);
 		StringBuilder sb = new StringBuilder(input.length()/2);
+		char best_seen = ('z' + 1) ;
+		int best_index = 0;
 		for(int i = 0; i < input.length(); i++) {
 			char letter =  input.charAt(input.length() - i - 1);
 			int index = letter - 'a';
+			if(letter < best_seen && index >= smallest) {
+				best_seen = letter;
+				best_index = i;
+				if(debugMode) 
+					System.out.println("best: " + best_seen + " index: " + best_index);
+				
+			}
 			if(debugMode) 
 				System.out.println("letter: " + letter + " index: " + index + " letterCounts: " + letterCounts[index] + " requiredCounts: " + requiredCounts[index]);
 			//Decrement the letter we are looking at.
 			letterCounts[index]--;
 			//Look to see if we need to add letter.
-			if(requiredCounts[index] > 0 && letterCounts[index] < requiredCounts[index]) {
-				//Then I have to take that letter.
-				--requiredCounts[index];
-				sb.append(letter);
-				if(debugMode) 
-					System.out.println("need to append: " + letter);
+			if(index != smallest && requiredCounts[index] > 0 && letterCounts[index] < requiredCounts[index]) {
+				//Then I have to take that letter. Only if we didn't skip a better letter.
+				if(letter == best_seen) {
+					--requiredCounts[index];
+					sb.append(letter);
+					best_seen = ('z' + 1) ;
+					if(debugMode) 
+						System.out.println("need to append: " + letter);
+					
+				} else {
+					//I need to go back and set the best letter.
+					while(i > best_index) {
+						if(debugMode) 
+							System.out.println("i: " + i + " best: " + best_index);
+						//Reset counts
+						letter =  input.charAt(input.length() - i - 1);
+						index = letter - 'a';
+						letterCounts[index]++;
+						i--;
+					}
+					letter =  input.charAt(input.length() - i - 1);
+					index = letter - 'a';
+					best_seen = ('z' + 1) ;
+					--requiredCounts[index];
+					sb.append(letter);
+					if(debugMode) 
+						System.out.println("went back to append: " + letter);
+				}
 			} else if(index == smallest){
 				//Should always take the smallest letter if I can.
 				sb.append(letter);
 				--requiredCounts[index];
 				smallest = smallest(requiredCounts);
+				best_seen = ('z' + 1) ;
 				if(debugMode) {
 					System.out.println("append smallest: " + letter);
 					System.out.println("smallest: " + smallest);
