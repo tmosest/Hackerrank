8de8195815dcce852b3f455b0155b3adc1bf7f9a
Moses <tmosest@gmail.com>
2017-01-10 10:36:34 -0500

    The Great Xor: Big Integer Version.

--- Algorithms/WeekOfCode/Week28/TheGreatXOR.java
+++ Algorithms/WeekOfCode/Week28/TheGreatXOR.java
@@ -1,7 +1,12 @@
 package WeekOfCode.Week28;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
+/**
+ *	Algorithms -> Week of Code 28 -> The Great XOR
+ *	Easy
+ */
 public class TheGreatXOR {
 
 	public static void main(String[] args) {
@@ -10,13 +15,25 @@ public class TheGreatXOR {
 		int qs = in.nextInt();
 		
 		for(int q = 0; q < qs; q++) {
-			long num = in.nextLong();
+			BigInteger num = in.nextBigInteger();
+			//long num = in.nextLong();
 			System.out.println(greatXorCount(num));
 		}
 		
 		in.close();
 	}
-
+	
+	public static BigInteger greatXorCount(BigInteger num)
+	{
+		BigInteger count = BigInteger.ZERO;
+		
+		for(BigInteger i = BigInteger.ONE; i.compareTo(num) < 0; i = i.add(BigInteger.ONE)) {
+			BigInteger xor = i.xor(num);
+			if(xor.compareTo(num) > 0) count = count.add(BigInteger.ONE);
+		}
+		
+		return count;
+	}
 	
 	public static long greatXorCount(long num)
 	{
