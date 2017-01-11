5a4d5361eb6613e351e8490971b47ba72176ea47
Moses <tmosest@gmail.com>
2017-01-11 12:19:23 -0500

    The Great XOR: found O(1) time.

--- Algorithms/WeekOfCode/Week28/TheGreatXOR.java
+++ Algorithms/WeekOfCode/Week28/TheGreatXOR.java
@@ -15,14 +15,57 @@ public class TheGreatXOR {
 		int qs = in.nextInt();
 		
 		for(int q = 0; q < qs; q++) {
-			BigInteger num = in.nextBigInteger();
-			//long num = in.nextLong();
-			System.out.println(greatXorCount(num));
+			//BigInteger num = in.nextBigInteger();
+			long num = in.nextLong();
+			System.out.println(greatXorCountConst(num));
 		}
 		
 		in.close();
+		
+		/*
+		for(int i = 1; i < 100; i++) {
+			System.out.println("i: " + i + " count : " + greatXorCountConst(i) + " pow 2: " + largestPowerOf2(i));
+		}
+		*/
 	}
 	
+	/**
+	 * Function to return the largest Power Of 2 that is less than a number
+	 * @param n
+	 * @return
+	 */
+	public static long largestPowerOf2(long n) {
+	    return (long) Math.pow(2, Math.floor(Math.log(n) / Math.log(2)));
+	}
+	
+	/**
+	 * Determines the great XOR count using long in constant time.
+	 * @param num
+	 * @return
+	 */
+	public static long greatXorCountConst(long num)
+	{
+		long count = 0;
+		
+		long powerOfTwo = largestPowerOf2(num);
+		
+		if(num == powerOfTwo)
+			return num - 1;
+		
+		
+		return 2 * powerOfTwo - 1 - num;
+	}
+	
+	public static BigInteger largestPowerOf2(BigInteger n)
+	{
+		
+	}
+	
+	/**
+	 * Determines the great XOR count using BigInteger in linear time.
+	 * @param num
+	 * @return
+	 */
 	public static BigInteger greatXorCount(BigInteger num)
 	{
 		BigInteger count = BigInteger.ZERO;
@@ -35,6 +78,11 @@ public class TheGreatXOR {
 		return count;
 	}
 	
+	/**
+	 * Determines the great XOR count using long in linear time.
+	 * @param num
+	 * @return
+	 */
 	public static long greatXorCount(long num)
 	{
 		long count = 0;
