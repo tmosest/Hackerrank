1460fb062737fdbb019e7cf3e0625b7bd9bf751d
Moses <tmosest@gmail.com>
2017-01-16 16:36:08 -0500

    Sam and SubString: solved some using a brute force method.

--- Algorithms/DynamicProgramming/SamAndSubStrings.java
+++ Algorithms/DynamicProgramming/SamAndSubStrings.java
@@ -1,5 +1,7 @@
 package DynamicProgramming;
 
+import java.util.Scanner;
+
 /**
  *	Algorithms -> Dynamic Programming -> Sam and sub-strings
  *	Medium
@@ -7,8 +9,35 @@ package DynamicProgramming;
 public class SamAndSubStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		String s = in.next();
+		
+		in.close();
+		
+		CountSumOfAllSubStrings ccs = new CountSumOfAllSubStrings(s);
+		System.out.println(ccs.getSum());
 	}
-
+	
+	public static class CountSumOfAllSubStrings
+	{
+		private static long mod = 1000000007;
+		private static long sum;
+		
+		public CountSumOfAllSubStrings(String s)
+		{
+			for(int i = 0; i < s.length(); i++) {
+				for(int j = i + 1; j <= s.length(); j++) {
+					long number = Long.parseLong(s.substring(i, j));
+					number %= mod;
+					sum += number;
+					sum %= mod;
+				}
+			}
+		}
+		
+		public long getSum()
+		{ return this.sum; }
+		
+	}// end CountSumOfAllSubStrings
 }
