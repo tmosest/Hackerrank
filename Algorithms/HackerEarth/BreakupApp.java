68c23df6f2d8d9b9fcc951d30eb1ffe0917c36c6
Moses <tmosest@gmail.com>
2017-01-20 12:10:18 -0500

    BreakupApp: working on it.

--- Algorithms/HackerEarth/BreakupApp.java
+++ Algorithms/HackerEarth/BreakupApp.java
@@ -18,15 +18,23 @@ public class BreakupApp {
 		
 		int[] counts = new int[101];
 		StringBuilder number = new StringBuilder();
-		
+		in.nextLine();
 		for(int q = 0; q < qs; q++) {
-			String s = in.next();
+			String s = in.nextLine();
+			System.out.println(s);
 			boolean isGirl = (s.charAt(0) == 'G');
+			System.out.println(isGirl);
 			for(int i = 0; i < s.length(); i++) {
 				char letter = s.charAt(i);
-				System.out.println(letter);
+				//System.out.println(letter);
 				if(letter >= '0' && letter <= '9') {
 					number.append(s);
+					if(i == s.length() - 1) {
+						int num = Integer.valueOf(number.toString());
+						counts[num]++;
+						if(isGirl) counts[num]++;
+						number = new StringBuilder();
+					}
 				} else {
 					if(number.length() > 0) {
 						int num = Integer.valueOf(number.toString());
