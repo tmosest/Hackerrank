244dce0e4c70b568d449dc10d5443829054bde70
Moses <tmosest@gmail.com>
2017-01-25 07:19:20 -0500

    Compare the Triplets: Solved.

---
+++ Algorithms/Warmups/CompareTheTriplets.js
@@ -0,0 +1,41 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+   var A = readLine().split(' ');
+   A = A.map(Number);
+   
+   var B = readLine().split(' ');
+   B = B.map(Number);
+   
+   var a = 0, b = 0;
+   
+   for(var i = 0; i < A.length; i++) {
+	   if(A[i] > B[i]) {
+		   a++;
+	   } else if(B[i] > A[i]) {
+		   b++
+	   }
+   }
+   
+   console.log(a + " " + b);
+}
