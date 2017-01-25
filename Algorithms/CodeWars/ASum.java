82f2cae0eb76e49e658e96e111b9642122116231
Moses <tmosest@gmail.com>
2017-01-25 13:35:55 -0500

    ASum: solved.

---
+++ Algorithms/CodeWars/ASum.java
@@ -0,0 +1,19 @@
+package CodeWars;
+
+public class ASum {
+	
+	public static long findNb(long m) {
+		long n = 0;
+		long sum = 0;
+		
+		while(sum < m) {
+			n++;
+			sum += (n * n * n);
+		}
+		
+		if(sum != m) 
+			n = -1;
+		
+		return n;
+	}	
+}
