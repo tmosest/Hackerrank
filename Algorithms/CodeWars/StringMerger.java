27f3878b8298d3cd3e85c9ca9786a5e196fc0718
Moses <tmosest@gmail.com>
2017-01-28 20:59:54 -0500

    String Merger: not solved.

---
+++ Algorithms/CodeWars/StringMerger.java
@@ -0,0 +1,55 @@
+package CodeWars;
+/**
+ * Decode the Morse code, advanced
+ * @author tmosest
+ */
+public class StringMerger {
+
+	private static final boolean debugMode = true;
+	
+    public static boolean isMerge(String s, String part1, String part2) {
+    	
+    	if(debugMode) 
+    		System.out.println("s: " + s);
+    	
+    	if(part1.length() + part2.length() < s.length())
+    		return false;
+    	
+    	int sIndex = 0, 
+    			part1Index = 0, part2Index = 0, 
+    			part1Last = -1, part2Last = -1;
+    	
+    	char sLetter, part1Letter, part2Letter;
+    	
+    	while(part1Last < part1.length() - 1 && part2Last < part2.length() - 1) {
+	    	sLetter = s.charAt(sIndex);
+	    	if(part1Index < part1.length()) {
+	    		part1Letter = part1.charAt(part1Index);
+	    		if(debugMode)
+	    			System.out.println("sLetter: " + sLetter + " part1Letter: " + part1Letter);
+	    		if(part1Letter == sLetter) {
+	    			part1Last = part1Index;
+	    			sIndex++;
+	    			part1Index++;
+	    			part2Index = part2Last + 1;
+	    		}
+	    	}
+	    	if(part2Index < part2.length()) {
+	    		part2Letter = part2.charAt(part2Index);
+	    		if(debugMode)
+	    			System.out.println("sLetter: " + sLetter + " part2Letter: " + part2Letter);
+	    		if(part2Letter == sLetter) {
+	    			part2Last = part2Index;
+	    			sIndex++;
+	    			part2Index++;
+	    			part1Index = part1Last + 1;
+	    		}
+	    	}
+	    	if(sIndex == s.length() - 1)
+	    		return true;
+    	}
+    	
+        return false;
+    }
+
+}
