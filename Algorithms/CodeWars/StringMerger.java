d2cd2d769b0fd974c124728b8f9e51b63ce9a064
Moses <tmosest@gmail.com>
2017-01-30 12:05:21 -0500

    String Merger: new idea can't test yet.

--- Algorithms/CodeWars/StringMerger.java
+++ Algorithms/CodeWars/StringMerger.java
@@ -15,37 +15,37 @@ public class StringMerger {
     	if(part1.length() + part2.length() < s.length())
     		return false;
     	
-    	int sIndex = 0, 
-    			part1Index = 0, part2Index = 0, 
-    			part1Last = -1, part2Last = -1;
+    	int sIndex = s.length() - 1, 
+    			part1Index = part1.length() - 1, part2Index = part2.length() - 1, 
+    			part1Last = part1.length(), part2Last = part2.length();
     	
     	char sLetter, part1Letter, part2Letter;
     	
-    	while(part1Last < part1.length() && part2Last < part2.length()) {
-	    	sLetter = s.charAt(sIndex);
-	    	if(part1Index < part1.length()) {
+    	while(part1Last > 0 && part2Last > 0) {
+    		sLetter = s.charAt(sIndex);
+    		if(part1Index > 0) {
 	    		part1Letter = part1.charAt(part1Index);
 	    		if(debugMode)
 	    			System.out.println("sLetter: " + sLetter + " part1Letter: " + part1Letter);
 	    		if(part1Letter == sLetter) {
 	    			part1Last = part1Index;
-	    			sIndex++;
-	    			part1Index++;
-	    			part2Index = part2Last + 1;
+	    			sIndex--;
+	    			part1Index--;
+	    			part2Index = part2Last - 1;
 	    		}
 	    	}
-	    	if(part2Index < part2.length()) {
+    		if(part2Index > 0) {
 	    		part2Letter = part2.charAt(part2Index);
 	    		if(debugMode)
 	    			System.out.println("sLetter: " + sLetter + " part2Letter: " + part2Letter);
 	    		if(part2Letter == sLetter) {
 	    			part2Last = part2Index;
-	    			sIndex++;
-	    			part2Index++;
-	    			part1Index = part1Last + 1;
+	    			sIndex--;
+	    			part2Index--;
+	    			part1Index = part1Last - 1;
 	    		}
 	    	}
-	    	if(sIndex == s.length() - 1)
+	    	if(sIndex == 0)
 	    		return true;
     	}
     	
