6c94c8b39b7c4d99acc8ff7607843e81867098b4
Moses <tmosest@gmail.com>
2017-01-31 15:51:00 -0500

    Simple Array Sum: solved.

---
+++ Algorithms/Warmup/SimpleArraySum.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import sys
+
+def sumArray(arr):
+    sum = 0
+    for number in arr:
+        sum += number
+    return sum
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+print(sumArray(arr))
