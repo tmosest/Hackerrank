0b6afbb1ac7a34a3ff5e8b94f36d2017bb61945c
AF17783 <tmosest@gmail.com>
2017-05-16 13:19:18 -0400

    Week 30: Poles.

---
+++ Algorithms/WeekOfCode/Week30/Poles.java
@@ -0,0 +1,23 @@
+package WeekOfCode.Week30;
+
+import java.util.Scanner;
+
+/**
+ *	Algorithms -> Week of Code 30 -> Poles
+ *	Hard
+ */
+public class Poles {
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		
+		int numTowers = in.nextInt();
+		int numStacks = in.nextInt();
+		
+		in.close();
+	}
+	
+	
+	
+} // end class Poles
